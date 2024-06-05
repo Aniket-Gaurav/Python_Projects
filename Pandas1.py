7bb9acd50362d362b23ae40e5b4c06acb72bea5e
Aniket Gaurav <aniketgaurav006@gmail.com>
2024-06-05 13:25:50 +0530

    Pandas Project

---
+++ Pandas1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+data = {
+    'Product' : ['A','B','C','D'],
+    'Sales' : [150,200,300,400],
+    'Profit' : [50,70,100,130]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+print(df['Sales'])
+
+high_sales = df[df['Sales']>200]
+print(high_sales)
+
+df['Revenue'] = df['Sales'] * df['Profit']
+print(df)
+
+df = df.drop('Revenue', axis=1)
+print(df)
+
+grouped = df.groupby('Product').sum()
+print(grouped)
+
+pivot = df.pivot_table(values='Sales', index='Product', aggfunc='sum')
+print(pivot)
