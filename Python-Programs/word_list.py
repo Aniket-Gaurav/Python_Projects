f9e4cec70c5c11c9f9d09bb1d17a7535e9a9399e
Aniket Gaurav <aniketgaurav006@gmail.com>
2025-03-05 21:47:49 +0530

    Hangman Game

---
+++ Python-Programs/word_list.py
@@ -0,0 +1,13 @@
+#word_list for hangman
+words = [
+    "python",
+    "java",
+    "ruby",
+    "json",
+    "javascript",
+    "c++",
+    "swift",
+    "objective-c",
+    "assembly",
+    "html"
+         ]
