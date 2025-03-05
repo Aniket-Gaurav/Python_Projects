a3b2ab6c45f7f96ca5598e84f93f16c6a0029d4c
Aniket Gaurav <aniketgaurav006@gmail.com>
2025-03-05 21:50:15 +0530

    Prime Checker

---
+++ Python-Programs/functions/prime_ checker.py
@@ -0,0 +1,18 @@
+import math
+
+
+def prime_checker(number):
+    is_prime = True
+    if number == 1:
+        is_prime = False
+    for i in range(2, math.ceil(number) + 1):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It is a prime number.")
+    else:
+        print("It is not a prime number.")
+
+
+n = int(input("Enter a number: "))
+prime_checker(n)
